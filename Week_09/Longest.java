8e6957643889cdf281e624db306b0b96527f3281
liutianyou <tianyou8368@163.com>
2020-09-13 23:14:39 +0800

    week_02_homework

---
+++ Week_09/Longest.java
@@ -0,0 +1,27 @@
+/**
+ * @author Liutianyou
+ * @date 2020/9/13 10:58 下午
+ */
+public class Longest {
+  public String longestPalindrome(String s) {
+    int n = s.length();
+    boolean[][] dp = new boolean[n][n];
+    String ans = "";
+    for (int l = 0; l < n; ++l) {
+      for (int i = 0; i + l < n; ++i) {
+        int j = i + l;
+        if (l == 0) {
+          dp[i][j] = true;
+        } else if (l == 1) {
+          dp[i][j] = (s.charAt(i) == s.charAt(j));
+        } else {
+          dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]);
+        }
+        if (dp[i][j] && l + 1 > ans.length()) {
+          ans = s.substring(i, i + l + 1);
+        }
+      }
+    }
+    return ans;
+  }
+}
